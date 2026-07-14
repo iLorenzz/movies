e55fd1e8323c15a8607bf844538fd2d98075ea98
iLorenzz <ldutra0912@gmail.com>
2026-07-14 03:29:17 -0300

    feat: implement rating model

--- search_movie_service/search_api/movie_api/models.py
+++ search_movie_service/search_api/movie_api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+
+class Rating(models.Model):
+    score = models.PositiveSmallIntegerField()
+
+    user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='ratings'
+    )
+
+    movie_id = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ('user_id', 'movie_id')
+
+
