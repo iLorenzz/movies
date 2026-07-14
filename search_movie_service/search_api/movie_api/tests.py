7647d27f5f1e817f971df829ec7c718741d68cff
iLorenzz <ldutra0912@gmail.com>
2026-07-14 15:30:38 -0300

    feat: implements unit test to calling serch/movie url from TMDB api

--- search_movie_service/search_api/movie_api/tests.py
+++ search_movie_service/search_api/movie_api/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .movie_service import TMDBClient
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+class TestMovieService(APITestCase):
+    def test_serach_movie_by_text_returns_200(self):
+        print("Mehtod: test_search_by_text")
+
+        client = TMDBClient()
+
+        response = client.search_movies_by_text('matrix', 1)
+        print(response)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
